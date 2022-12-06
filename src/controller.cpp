20ba69cd00e1147b7b16a21780a3f2b4c1452b3a
Michael Meixner <michael.meixner@lmco.com>
2022-12-06 13:56:58 -0500

    quit game on END keystroke

--- src/controller.cpp
+++ src/controller.cpp
@@ -43,6 +43,9 @@ void Controller::HandleInput(bool &running, Snake &snake) const
         ChangeDirection(snake, Snake::Direction::kRight,
                         Snake::Direction::kLeft);
         break;
+      case SDLK_END:
+        std::cout << "QUIT" << std::endl;
+        running = false;
       }
     }
   }
